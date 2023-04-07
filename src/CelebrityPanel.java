2dcdefbe59d19177c640bc568ddb1876e7c02991
Sneeky Stryder <ifazilov3584@bths.edu>
2023-04-07 18:04:21 -0400

    part 3 done

--- src/CelebrityPanel.java
+++ src/CelebrityPanel.java
@@ -110,6 +110,7 @@ public class CelebrityPanel extends JPanel implements ActionListener {
     success = "You guessed correctly!!! \nNext Celebrity clue is: ";
     tryAgain = "You have chosen poorly, try again!\nThe clue is: ";
     seconds = 60;
+    countdownTimer = new Timer(1000, null);
 
     setupPanel();
     setupLayout();
@@ -177,6 +178,8 @@ public class CelebrityPanel extends JPanel implements ActionListener {
    */
   private void setupListeners() {
     guessButton.addActionListener(this);
+    countdownTimer.addActionListener(this);
+    countdownTimer.start();
   }
   
   /**
@@ -185,7 +188,15 @@ public class CelebrityPanel extends JPanel implements ActionListener {
    * the end.
    */
   private void timerFires() {
-
+    seconds--;
+    dynamicTimerLabel.setText("" + seconds);
+    if (seconds == 0) {
+      countdownTimer.stop();
+      staticTimerLabel.setText("Time's up! YOU LOSE");
+      dynamicTimerLabel.setText("");
+      guessButton.setEnabled(false);
+      guessField.setEnabled(false);
+    }
   }
   
   /**
@@ -196,6 +207,9 @@ public class CelebrityPanel extends JPanel implements ActionListener {
    */
   public void addClue(String clue) {
     clueArea.setText("The clue is: " + clue);
+    seconds = 30;
+    dynamicTimerLabel.setText("" + seconds);
+    countdownTimer.restart();
   }
   
   /**
@@ -218,6 +232,9 @@ public class CelebrityPanel extends JPanel implements ActionListener {
 
     if (controller.getCelebrityGameSize() == 0) {
       clueArea.append("\nNo more celebrities to guess.");
+      countdownTimer.stop();
+      staticTimerLabel.setText("YOU WIN!");
+      dynamicTimerLabel.setText("");
       guessButton.setEnabled(false);
       guessButton.setEnabled(false);
     }
@@ -225,6 +242,16 @@ public class CelebrityPanel extends JPanel implements ActionListener {
 
   @Override
   public void actionPerformed(ActionEvent e) {
-
+    Object source = e.getSource();
+    if (source instanceof Timer) {
+      timerFires();
+    }
+    else if (source instanceof JButton) {
+      JButton clickedButton = (JButton) source;
+      String buttonText = clickedButton.getText();
+      if (buttonText.equals("Submit guess")) {
+        updateScreen();
+      }
+    }
   }
 }
